73e018625ad0e24c4020b7194221d195a2dae429
mmt <pycdremail@gmail.com>
2022-02-28 21:46:53 +0330

    added token generator and fixed bugs

--- ptbtest/mocks/mock_updater.py
+++ ptbtest/mocks/mock_updater.py
@@ -1,17 +1,26 @@
 import logging
 from queue import Queue
 from threading import Event, Lock, Thread
-from typing import List
+from typing import Any, List
 from telegram.ext import Dispatcher, JobQueue, Updater
 from .mock_request import MockRequest
 from .mock_bot import MockBot
+import random, string
+
+def generate_random_token():
+    left = "".join(map(str, random.choices(string.digits, k=9)))
+    right = "".join(map(str, random.choices(string.ascii_letters+string.digits, k=35)))
+    return f"{left}:{right}"
 
 class MockUpdater(Updater):
     """Mock Updater based on `telegram.Updater`"""
-    def __init__(self):
+    def __init__(self, token: str = None):
         self.logger = logging.getLogger(__name__)
         self._request = MockRequest()
-        self.bot = MockBot(request = self._request)
+        self.bot = MockBot(
+            request = self._request, 
+            token = token or generate_random_token()
+        )
         self.update_queue: Queue = Queue()
         self.job_queue = JobQueue()
         self.__exception_event = Event()
@@ -33,4 +42,10 @@ class MockUpdater(Updater):
         self.is_idle = False
         self.httpd = None
         self.__lock = Lock()
-        self.__threads: List[Thread] = []    
+        self.__threads: List[Thread] = []
+    
+    def __getattribute__(self, __name: str) -> Any:
+        if __name.startswith("_Updater"):
+            # to fix "AttributeError: self._Updater__lock"
+            return object.__getattribute__(self, f"_MockUpdater{__name[8:]}")
+        return object.__getattribute__(self, __name)
