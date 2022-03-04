276ce3ea30af154ec25664f5aaf16d35ec1c1c06
mmt <pycdremail@gmail.com>
2022-03-04 15:07:08 +0330

    setup updated

--- setup.py
+++ setup.py
@@ -1,37 +1,34 @@
-import codecs
 from distutils.core import setup
-
 from setuptools import find_packages
 
-with codecs.open("readme.rst", "r", "utf-8") as fd:
-
+with open("README.rst", 'r') as readme_file:
     setup(
         name='ptbtest',
-        version='1.3',
-        packages=['ptbtest'],
-        url='https://github.com/Eldinnie/ptbtest',
+        version='2.0',
+        packages=['ptbtest', 'ptbtest.mocks'],
+        url='https://github.com/python-telegram-bot/ptbtest',
         license='GNU General Public License v3.0',
-        author='Pieter Schutz',
-        author_email='pieter.schutz@gmail.com',
-        description='A testsuit for use with python-telegram-bot',
-        long_description=fd.read(),
-        install_requires=['python-telegram-bot'],
-        keywords='python telegram bot unittest',
+        author='M.M. Tahmasbi',
+        author_email='pycdremail@gmail.com',
+        description='A test library for python-telegram-bot (ptb) package',
+        long_description=readme_file.read(),
+        install_requires=['python-telegram-bot', 'pytest'],
+        keywords='python telegram bot unittest pytest test ptb ptbtest',
         classifiers=[
-                  'Development Status :: 4 - Beta',
-                  'Intended Audience :: Developers',
-                  'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
-                  'Operating System :: OS Independent',
-                  'Topic :: Software Development :: Libraries :: Python Modules',
-                  'Topic :: Software Development :: Testing',
-                  'Topic :: Internet',
-                  'Programming Language :: Python',
-                  'Programming Language :: Python :: 2',
-                  'Programming Language :: Python :: 2.7',
-                  'Programming Language :: Python :: 3',
-                  'Programming Language :: Python :: 3.3',
-                  'Programming Language :: Python :: 3.4',
-                  'Programming Language :: Python :: 3.5',
-                  'Programming Language :: Python :: 3.6'
-              ],
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+            'Operating System :: OS Independent',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Topic :: Software Development :: Testing',
+            'Topic :: Internet',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6'
+        ],
     )
